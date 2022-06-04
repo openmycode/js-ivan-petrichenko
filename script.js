e0f3bc70f0752248b62114dc9751ac8c50a5ee76
Dany <mineralnone@gmail.com>
2022-06-04 09:46:09 +0300

    Task 2 >automation

--- script.js
+++ script.js
@@ -10,11 +10,26 @@ const personalMovieDB = {
 	privat: false,
 }
 
-let film = prompt('Один из последних просмотренных фильмов?');
-let filmScore = prompt('На сколько оцените его?');
-personalMovieDB.movies[film] = filmScore;
-film = prompt('Один из последних просмотренных фильмов?');
-filmScore = prompt('На сколько оцените его?');
-personalMovieDB.movies[film] = filmScore;
+switch (true) {
+	case personalMovieDB.count < 10:
+		console.log("Просмотренно слишком мало фильмов!");
+		break;
+	case personalMovieDB.count >= 10 && personalMovieDB.count < 30: 
+		console.log("Вы классический зритель");
+		break;
+	default:
+		console.log("Вы настоящий киноман!");
+		break;
+}
+
+for (let i = 0; i < 2; i++) {
+	const film = prompt('Один из последних просмотренных фильмов?');
+	const filmScore = prompt('На сколько оцените его?');
+	if (film !== null && filmScore !== null && film !== '' && filmScore !== '' && film.length < 50) {
+		personalMovieDB.movies[film] = filmScore;
+	} else {
+		i--;
+	}
+}
 
 console.log(personalMovieDB);
